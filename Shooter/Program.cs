6e28dab5c1c2ba1e3fdd8077b5a8d275bcc586fb
Sania <Sania@DESKTOP-3HL58UN>
2023-11-09 23:45:07 +0800

    Shooter_DONE

---
+++ Shooter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo input;
+            Console.WriteLine("Тестируйте управление WASD, Space и Enter. По завершению нажмите Escape.");
+            do
+            {
+                input= Console.ReadKey(true);
+                if (input.Key == ConsoleKey.W) Console.WriteLine("Вперед");
+                if (input.Key == ConsoleKey.A) Console.WriteLine("Влево");
+                if (input.Key == ConsoleKey.D) Console.WriteLine("Вправо");
+                if (input.Key == ConsoleKey.S) Console.WriteLine("Вниз");
+                if (input.Key == ConsoleKey.Spacebar) Console.WriteLine("Прыжок");
+                if (input.Key == ConsoleKey.Enter) Console.WriteLine("Огонь");
+            }
+            while (input.Key != ConsoleKey.Escape);
+        }
+    }
+}
