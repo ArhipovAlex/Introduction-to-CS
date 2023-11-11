491a3f4e925845a7e164aff7a38afe9bd086bb13
Sania <Sania@DESKTOP-3HL58UN>
2023-11-11 20:52:25 +0800

    Calculator DONE

--- Shooter/Program.cs
+++ Shooter/Program.cs
@@ -16,9 +16,13 @@ namespace Shooter
             {
                 input= Console.ReadKey(true);
                 if (input.Key == ConsoleKey.W) Console.WriteLine("Вперед");
+                if (input.Key == ConsoleKey.UpArrow) Console.WriteLine("Вперед");
                 if (input.Key == ConsoleKey.A) Console.WriteLine("Влево");
+                if (input.Key == ConsoleKey.LeftArrow) Console.WriteLine("Влево");
                 if (input.Key == ConsoleKey.D) Console.WriteLine("Вправо");
+                if (input.Key == ConsoleKey.RightArrow) Console.WriteLine("Вправо");
                 if (input.Key == ConsoleKey.S) Console.WriteLine("Вниз");
+                if (input.Key == ConsoleKey.DownArrow) Console.WriteLine("Вниз");
                 if (input.Key == ConsoleKey.Spacebar) Console.WriteLine("Прыжок");
                 if (input.Key == ConsoleKey.Enter) Console.WriteLine("Огонь");
             }
