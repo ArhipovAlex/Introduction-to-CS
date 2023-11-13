a0f568b03e126d654990dbafcf0ec68fa028bee2
Sania <Sania@DESKTOP-3HL58UN>
2023-11-14 00:00:05 +0800

    Graphic_Shooter_v1.0_DONE

--- Shooter/Program.cs
+++ Shooter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,25 +9,52 @@ namespace Shooter
 {
     internal class Program
     {
+            public static int x;//координаты игрока
+            public static int y;
         static void Main(string[] args)
         {
+
             ConsoleKeyInfo input;
-            Console.WriteLine("Тестируйте управление WASD, Space и Enter. По завершению нажмите Escape.");
+            start_game();//запускаем новую игру
             do
             {
-                input= Console.ReadKey(true);
-                if (input.Key == ConsoleKey.W) Console.WriteLine("Вперед");
-                if (input.Key == ConsoleKey.UpArrow) Console.WriteLine("Вперед");
-                if (input.Key == ConsoleKey.A) Console.WriteLine("Влево");
-                if (input.Key == ConsoleKey.LeftArrow) Console.WriteLine("Влево");
-                if (input.Key == ConsoleKey.D) Console.WriteLine("Вправо");
-                if (input.Key == ConsoleKey.RightArrow) Console.WriteLine("Вправо");
-                if (input.Key == ConsoleKey.S) Console.WriteLine("Вниз");
-                if (input.Key == ConsoleKey.DownArrow) Console.WriteLine("Вниз");
-                if (input.Key == ConsoleKey.Spacebar) Console.WriteLine("Прыжок");
-                if (input.Key == ConsoleKey.Enter) Console.WriteLine("Огонь");
+                draw();//рисуем первый кадр или перерисовываем кадр с новыми координатами 
+                input= Console.ReadKey(true);//перехватываем кнопку управления и меняем координаты объекта игрока
+                if (input.Key == ConsoleKey.W) y--;
+                if (input.Key == ConsoleKey.UpArrow) y--;
+                if (input.Key == ConsoleKey.A) x--;
+                if (input.Key == ConsoleKey.LeftArrow) x--;
+                if (input.Key == ConsoleKey.D) x++;
+                if (input.Key == ConsoleKey.RightArrow) x++;
+                if (input.Key == ConsoleKey.S) y++;
+                if (input.Key == ConsoleKey.DownArrow) y++;
             }
             while (input.Key != ConsoleKey.Escape);
+            Console.Clear();
+        }
+        private static void draw()//рисуем или обновляем игровое поле чтобы отобразить изменения
+        {
+            Console.Clear();
+            player_draw();
+        }
+        private static void start_game()//запускаем новую игру
+        {
+            Random rnd = new Random();
+            x = rnd.Next(20,50);
+            y = rnd.Next(20,50);
+            draw();
+        }
+        private static void player_draw()//рисуем управляемый игроком объект
+        {
+            for (int j = 0; j < y; j++) Console.WriteLine();
+            for (int i = 0; i < x; i++) Console.Write(" ");
+            Console.Write("XXXXX");
+            Console.WriteLine();
+            for (int i = 0; i < x; i++) Console.Write(" ");
+            Console.Write("XXXXX");
+            Console.WriteLine();
+            for (int i = 0; i < x; i++) Console.Write(" ");
+            Console.Write("XXXXX");
         }
     }
 }
